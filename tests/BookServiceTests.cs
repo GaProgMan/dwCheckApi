96d4bf04b982de63ad079962a1fedffdb632f122
GaProgMan <jamie@gaprogman.com>
2017-06-13 22:10:25 +0100

    moved unit tests for database seeder out to their own class

--- tests/BookServiceTests.cs
+++ tests/BookServiceTests.cs
@@ -27,6 +27,8 @@ namespace dwCheckApi.Tests
         [Fact]
         public void BookService_GetBookByOrdinal_ReturnsOneBook()
         {
+            // What is this test even testing?
+            
             // Arrange
             var idForTest = 1;
             var dbBook = GetTestBookById(idForTest);
@@ -42,56 +44,11 @@ namespace dwCheckApi.Tests
             Assert.Equal(testViewModel.BookIsbn13, viewModel.BookIsbn13);
         }
 
-        // [Fact]
-        // public void DbSeeder_SeedBookData_NoDataSupplied()
-        // {
-        //     // Arrange
-        //     var mockBookSet = new Mock<DbSet<Book>>();
-        //     var mockset = new Mock<DwContext>();
-        //     mockset.Setup(m => m.Books).Returns(mockBookSet.Object);
-
-        //     // Act & Assert
-        //     var dbSeeder = new DatabaseSeeder(mockset.Object);
-        //     ArgumentException argEx = Assert.Throws<ArgumentException>(() =>
-        //         dbSeeder.SeedBookEntitiesFromJson(string.Empty));
-        //     Assert.Contains("must be supplied to", argEx.Message);
-        // }
-
-        // [Fact]
-        // public void DbSeeder_SeedBookData_DataSupplied()
-        // {
-        //     // Arrange
-        //     // TODO Add an interface here, to mock stuff properly
-        //     var bookList = new List<Book>();
-        //     var mockBookSet = GetQueryableDbSet(bookList);
-        //     var mockset = new Mock<IDwContext>();
-        //     mockset.Setup(m => m.Books).Returns(mockBookSet.Object);
-        //     var testJsonDirectory = Path.Combine(Directory.GetCurrentDirectory(), "SeedData");
-        //     if (!Directory.Exists(testJsonDirectory))
-        //     {
-        //         output.WriteLine("SeedData directory does not exist!");
-        //     }
-        //     if (File.Exists(Path.Combine(testJsonDirectory, "TestBookSeedData.json")))
-        //     {
-        //         foreach(var file in Directory.GetFiles(testJsonDirectory))
-        //         {
-        //             output.WriteLine(file);
-        //         }
-        //     }
-        //     var pathToSeedData = Path.Combine(testJsonDirectory, "TestBookSeedData.json");
-        //     var dbSeeder = new DatabaseSeeder(mockset.Object);
-            
-        //     // Act
-        //     var addedEntitiesCount = dbSeeder.SeedBookEntitiesFromJson(pathToSeedData);
-
-        //     // Assert
-        //     Assert.NotEqual(0, addedEntitiesCount);
-        // }
-
         private Book GetTestBookById(int id)
         {
             return GetTestBooks().FirstOrDefault(b => b.BookId == id);
         }
+        
         private List<Book> GetTestBooks()
         {
             var mockData = new List<Book>();
@@ -122,19 +79,5 @@ namespace dwCheckApi.Tests
 
             return viewModels;
         }
-
-        // private static Mock<DbSet<T>> GetQueryableDbSet<T>(List<T> sourceList) where T : class
-        // {
-        //     var queryable = sourceList.AsQueryable();
-
-        //     var dbSet = new Mock<DbSet<T>>();
-        //     dbSet.As<IQueryable<T>>().Setup(m => m.Provider).Returns(queryable.Provider);
-        //     dbSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(queryable.Expression);
-        //     dbSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(queryable.ElementType);
-        //     dbSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(() => queryable.GetEnumerator());
-        //     dbSet.Setup(d => d.Add(It.IsAny<T>())).Callback<T>((s) => sourceList.Add(s));
-
-        //     return dbSet;
-        // }
-    }
+    }   
 }
