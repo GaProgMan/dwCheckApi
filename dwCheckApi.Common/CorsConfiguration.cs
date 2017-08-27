f5b887797240b5fbd969ec2116abae7fa00c503d
Jamie Taylor <seppydude@gmail.com>
2017-08-27 14:31:47 +0100

    Missing from previous commit

--- dwCheckApi.Common/CorsConfiguration.cs
+++ dwCheckApi.Common/CorsConfiguration.cs
@@ -1,13 +1,17 @@
-﻿namespace dwCheckApi.Common
+﻿using System;
+namespace dwCheckApi.Common
 {
     public class CorsConfiguration : ConfigurationBase
     {
+        private string CorsPolicyKey = "CorsPolicy:name";
         public string GetCorsPolicyName()
         {
-            var section = GetConfiguration().GetSection("CorsPolicy:name");
-
-            return section?["name"];
-
+            var section = GetConfiguration().GetSection(CorsPolicyKey);
+            if (section == null)
+            {
+                RaiseValueNotFoundException(CorsPolicyKey);
+            }
+            return section.Value;
         }
     }
 }
